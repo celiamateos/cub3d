be9dfd683c01dc28caec1e66bd9a22b648429040
settes <ioanaroxana.st@gmail.com>
2024-10-21 20:23:00 +0200

    update

--- src/map_draw/draw_map.c
+++ src/map_draw/draw_map.c
@@ -6,7 +6,7 @@
 /*   By: iostancu <iostancu@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/17 22:11:22 by iostancu          #+#    #+#             */
-/*   Updated: 2024/10/18 00:42:39 by iostancu         ###   ########.fr       */
+/*   Updated: 2024/10/18 00:45:17 by iostancu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -86,7 +86,21 @@ void	draw_border_map(t_map *map, mlx_image_t *screen)
 	}
 }
 
+int	**map_to_int(char **map, int h, int w)
+{
+	int	**grid;
+	int	i;
+	int	j;
+	int	k;
 
+	i = 0;
+	j = 0;
+	grid = malloc(sizeof(int *) * (h * w));
+	if (!grid)
+		return (NULL);
+	whi
+	return (grid);
+}
 
 /**
  * @brief Minimap. First draw a border, then draw the map.
@@ -102,16 +116,11 @@ void	draw_2d_map(t_map *map, mlx_image_t *screen)
 
 	draw_border_map(map, screen);
 	y = SIZE + 1;
-	printf("hellooooooo\n");
-	printf("map->height: %d\n", map->height);
-	printf("map->width: %d\n", map->width);
 	while (y < map->height)
 	{
 		x = SIZE + 1;
-		printf("y: %d\n", y);
 		while (x < map->width)
 		{
-			printf("x: %d\n", x);
 			if (map->grid[y][x] == 1)
 				draw_cube(screen, (t_vec2){x * SIZE, y * SIZE}, 0x00FF00);
 			else if (map->grid[y][x] == 0)
